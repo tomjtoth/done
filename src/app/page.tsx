f5fae8a7e32bb51ea1c4fbfb78ebe02805d8fbc2
Tamás Tóth <tomjtoth@gmail.com>
2025-04-09 12:43:36 +0300

    simplified Home view of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/users">Users View</Link>
-      </li>
-    </ul>
+    <>
+      <p>
+        This is a multi-user activity tracker to get things <em>done</em>!
+      </p>
+    </>
   );
 }
