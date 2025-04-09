823339271401c88a209690500675b7b76ee4ef76
Tamás Tóth <tomjtoth@gmail.com>
2025-04-09 12:44:13 +0300

    reworked users view

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,6 @@
-import { createUser, getUsers } from "@/app/actions";
+import { getUsers } from "@/app/actions";
 
-export default async function Users() {
+export default async function ListUsers() {
   const users = await getUsers();
   return (
     <>
@@ -12,27 +12,6 @@ export default async function Users() {
           </li>
         ))}
       </ul>
-
-      <h3>Create new user</h3>
-      <form className="flex flex-col items-center *:border *:p-2 p-2 gap-2 [&_label]:pr-2">
-        <div>
-          <label htmlFor="name">name:</label>
-          <input type="text" name="name" />
-        </div>
-
-        <div>
-          <label htmlFor="email">email:</label>
-          <input type="text" name="email" />
-        </div>
-
-        <div>
-          <label htmlFor="password">password:</label>
-          <input type="text" name="password" />
-        </div>
-        <button className="cursor-pointer" formAction={createUser}>
-          Register
-        </button>
-      </form>
     </>
   );
 }
