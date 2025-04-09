735b2553158db95acddf8e6bf794b54103ef7b9e
Tamás Tóth <tomjtoth@gmail.com>
2025-04-09 12:45:09 +0300

    added dynamic navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import { cookies } from "next/headers";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { logoutUser } from "./actions";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,16 +20,51 @@ export const metadata: Metadata = {
   description: "An activity tracker",
 };
 
-export default function RootLayout({
+type View = { href: string; label: string };
+
+const baseViews = [
+  { href: "/", label: "home" },
+  { href: "/users", label: "users" },
+  { href: "/events", label: "events" },
+];
+
+const anonymous = [
+  { href: "/login", label: "login" },
+  { href: "/register", label: "register" },
+];
+
+const listItems = (arr: View[]) =>
+  arr.map((v) => (
+    <li key={v.href}>
+      <Link href={v.href}>{v.label}</Link>
+    </li>
+  ));
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookie = (await cookies()).get("session");
+  const session = cookie ? JSON.parse(cookie.value) : null;
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <ul className="flex gap-2 bg-amber-300 *:select-none">
+            {listItems(baseViews)}
+            {session ? (
+              <button className="cursor-pointer" onClick={logoutUser}>
+                logout ({session.name})
+              </button>
+            ) : (
+              listItems(anonymous)
+            )}
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
