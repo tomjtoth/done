1469ed937c919d0de462a791c7145cddf739eada
Tamás Tóth <tomjtoth@gmail.com>
2025-04-09 12:42:58 +0300

    added login view

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+
+import { loginUser } from "@/app/actions";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function Users() {
+  return (
+    <form className="flex flex-col items-center *:border *:p-2 p-2 gap-2 [&_label]:pr-2">
+      <div>
+        <label htmlFor="email">email:</label>
+        <input type="text" name="email" />
+      </div>
+
+      <div>
+        <label htmlFor="password">password:</label>
+        <input type="text" name="password" />
+      </div>
+
+      <button className="cursor-pointer" formAction={loginUser}>
+        Login
+      </button>
+    </form>
+  );
+}
