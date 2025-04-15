10d56753f89e58eb26a337125e897ce165c02cee
Tamás Tóth <tomjtoth@gmail.com>
2025-04-15 20:41:02 +0300

    fixed linting #deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ export default function Home() {
             <h4>A03 - OWASP 2021</h4>
             <p>
               SQL injection is currently possible on this site: a malicious user
-              might alter some other user's name by creating an event with the
-              following content in the description field:{" "}
+              might alter some other user&apos;s name by creating an event with
+              the following content in the description field:{" "}
               <code className="bg-gray-300 font-mono text-lg">
-                description of new event'); update users set name='pwned' where
-                id=1; --
+                description of new event&apos;); update users set
+                name=&apos;pwned&apos; where id=1; --
               </code>
             </p>
           </li>
@@ -31,9 +31,9 @@ export default function Home() {
             <h4>A01 - OWASP 2021</h4>
             <p>
               Creating events and assigning other users as owners is currently
-              possible, actually it doesn't even require an active user session,
-              so an anonymous visitor can also create events in the system,
-              which is undesirable.
+              possible, actually it doesn&apos;t even require an active user
+              session, so an anonymous visitor can also create events in the
+              system, which is undesirable.
             </p>
           </li>
         )}
