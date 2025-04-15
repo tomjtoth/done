c3e33eaa52bbb6a66cdeab4ce59bc4725c0eafed
Tamás Tóth <tomjtoth@gmail.com>
2025-04-15 19:52:11 +0300

    added switch A01_2021

--- src/lib/vulnerabilities.ts
+++ src/lib/vulnerabilities.ts
@@ -4,6 +4,11 @@
 export const A03_2021 = true;
 
 /**
+ * https://owasp.org/Top10/A01_2021-Broken_Access_Control/
+ */
+export const A01_2021 = true;
+
+/**
  * https://owasp.org/Top10/A09_2021-Security_Logging_and_Monitoring_Failures/
  */
 export const A09_2021 = true;
