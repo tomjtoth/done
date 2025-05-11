d12d327b3146c3058e2fc77ef103e540aff56daa
Tamás Tóth <tomjtoth@gmail.com>
2025-05-11 14:49:27 +0300

    updated description, ready to #deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,8 @@ export default function Home() {
   return (
     <>
       <p className="m-2">
-        This is a{" "}
-        <s>
-          multi-user activity tracker to get things <em>done</em>
-        </s>{" "}
-        fake app for a course at the University of Helsinki, use at your own
-        risk!
+        This is a fake app with 5 known vulnerabilities for a course at the
+        University of Helsinki, (do not) use at your own risk!
       </p>
       <ul>
         {A03_2021 && (
