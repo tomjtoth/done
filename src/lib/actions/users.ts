238fb0fdd090008749df359dd1bbd3c88b87954b
Tamás Tóth <tomjtoth@gmail.com>
2025-05-11 14:14:30 +0300

    major refactors in user handling processes

--- src/lib/actions/users.ts
+++ src/lib/actions/users.ts
@@ -3,22 +3,36 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import SQL from "sql-template-strings";
+import { hashSync, compareSync } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import { getDb } from "@/lib/db";
-import { A09_2021 } from "../vulnerabilities";
+import { JWT_SECRET } from "@/lib/config";
+import { A02_2021, A09_2021 } from "../vulnerabilities";
 
-type User = { id?: number; name: string; email: string; password: string };
+type User = {
+  id?: number;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type SessUser = Omit<User, "password"> & {
+  token?: string;
+};
 
 export async function createUser(data: FormData) {
   const name = data.get("name") as string;
   const email = data.get("email") as string;
   const password = data.get("password") as string;
 
+  const pwHash = hashSync(password, 10);
+
   const db = await getDb();
   await db.run(SQL`insert into users (name, email, password)
-    values (${name}, ${email}, ${password});`);
+    values (${name}, ${email}, ${pwHash});`);
 
-  if (!A09_2021) console.log("new user registered:", { name, email, password });
+  if (!A09_2021) console.log("new user registered:", { name, email });
 
   redirect("/login");
 }
@@ -35,19 +49,36 @@ export async function loginUser(data: FormData) {
   const db = await getDb();
   const userRow = await db.get<User>(
     SQL`select * from users
-    where email = ${email} 
-    and password = ${password}`
+    where email = ${email}`
   );
 
-  if (userRow) {
-    const cStore = await cookies();
-    cStore.set("session", JSON.stringify(userRow), { path: "/" });
+  if (!userRow) {
+    if (!A09_2021) {
+      console.log("failed login attempt (non-existent user):", email);
+    }
 
-    if (!A09_2021) console.log("user logged in:", email);
-    redirect("/events");
-  } else if (!A09_2021) {
-    console.log("someone tried logging in via:", { email, password });
+    return;
   }
+
+  if (!compareSync(password, userRow.password)) {
+    if (!A09_2021) console.log("failed login attempt (wrong password):", email);
+
+    return;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: _, ...safeToSend } = userRow;
+
+  if (!A02_2021)
+    (safeToSend as SessUser).token = jwt.sign(safeToSend, JWT_SECRET);
+  const session = JSON.stringify(safeToSend);
+
+  const cStore = await cookies();
+  cStore.set("session", session, { path: "/" });
+
+  if (!A09_2021) console.log("user successfully logged in:", email);
+
+  redirect("/events");
 }
 
 export async function logoutUser() {
